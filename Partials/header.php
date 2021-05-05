5ffc0b9f4f4f42c00a2cf59d53e40dd945159b7f
Caterina Bertotti <bertotti4997@gmail.com>
2021-05-05 17:04:39 +0200

    UI

---
+++ Partials/header.php
@@ -0,0 +1,9 @@
+<body>
+    <div class="wrapper">
+        <header>
+            <div class="container">
+                <i class="fab fa-spotify"></i>
+            </div>
+        </header>
+    
+    
---
+++ Partials/main.php
@@ -0,0 +1,17 @@
+        <main>
+            <div class="container">
+                <div class="album">
+                    <div class="album__img"></div>
+                </div>
+                <div class="album">
+                    <div class="album__img"></div>
+                </div>
+                <div class="album">
+                    <div class="album__img"></div>
+                </div>
+                <div class="album">
+                    <div class="album__img"></div>
+                </div>
+            </div>
+        </main>
+    

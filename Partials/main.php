242f3805b5b37baf085f00036b92e41e0a657dee
Caterina Bertotti <bertotti4997@gmail.com>
2021-05-05 17:17:42 +0200

    UI 2

--- Partials/main.php
+++ Partials/main.php
@@ -1,16 +1,40 @@
         <main>
             <div class="container">
                 <div class="album">
-                    <div class="album__img"></div>
+                    <div class="album__img">
+                    </div>
+                    <div class="album__caption">
+                    <h3>Titolo</h3>
+                    <p class="author">Autore</p>
+                    <p class="year">2000</p>
+                    </div>
                 </div>
                 <div class="album">
-                    <div class="album__img"></div>
+                    <div class="album__img">
+                    </div>
+                    <div class="album__caption">
+                    <h3>Titolo</h3>
+                    <p class="author">Autore</p>
+                    <p class="year">2000</p>
+                    </div>
                 </div>
                 <div class="album">
-                    <div class="album__img"></div>
+                    <div class="album__img">
+                    </div>
+                    <div class="album__caption">
+                        <h3>Titolo</h3>
+                        <p class="author">Autore</p>
+                        <p class="year">2000</p>
+                    </div>
                 </div>
                 <div class="album">
-                    <div class="album__img"></div>
+                    <div class="album__img">
+                    </div>
+                    <div class="album__caption">
+                        <h3>Titolo</h3>
+                        <p class="author">Autore</p>
+                        <p class="year">2000</p>
+                    </div>
                 </div>
             </div>
         </main>
